e5e7c7fde3654ffa4a22e57d141bf541f1a4259e
bestia.dev <info@bestia.dev>
2022-05-11 12:04:16 +0000

    plantuml

--- examples/plantuml1.rs
+++ examples/plantuml1.rs
@@ -11,14 +11,21 @@ fn main() {
 
     // region: prepare folders and files for this example
     // empty the 'images' folder
-    std::fs::remove_dir_all ("examples/plantuml/images").unwrap();
+    std::fs::remove_dir_all("examples/plantuml/images").unwrap();
     std::fs::create_dir("examples/plantuml/images").unwrap();
     // copy md files from sample_data to examples
-    std::fs::copy("sample_data/input1_for_plantuml.md", "examples/plantuml/input1_for_plantuml.md").unwrap();
-    std::fs::copy("sample_data/input2_for_plantuml.md", "examples/plantuml/input2_for_plantuml.md").unwrap();
+    std::fs::copy(
+        "sample_data/input1_for_plantuml.md",
+        "examples/plantuml/input1_for_plantuml.md",
+    )
+    .unwrap();
+    std::fs::copy(
+        "sample_data/input2_for_plantuml.md",
+        "examples/plantuml/input2_for_plantuml.md",
+    )
+    .unwrap();
     // endregion: prepare folders and files for this example
 
     let path = std::path::Path::new("examples/plantuml");
     cargo_auto_lib::auto_plantuml_for_path(path);
-
 }
