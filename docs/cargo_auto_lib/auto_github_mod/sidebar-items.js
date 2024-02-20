2e1a2712a1173521f7728c799c12c43c75de665c
luciano_bestia <info@bestia.dev>
2024-02-20 16:27:25 +0000

    update docs

--- docs/cargo_auto_lib/auto_github_mod/sidebar-items.js
+++ docs/cargo_auto_lib/auto_github_mod/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["api_call_repository_new","ask_for_github_ssh_for_push","get_identity_from_ssh_config","init_repository_if_needed","new_local_repository","new_remote_github_repository","ssh_add_if_needed"]};
+window.SIDEBAR_ITEMS = {"fn":["ask_for_github_ssh_for_push","get_identity_from_ssh_config","github_api_create_new_release","github_api_repository_new","github_api_upload_asset_to_release","init_repository_if_needed","new_local_repository","new_remote_github_repository","ssh_add_if_needed"]};
