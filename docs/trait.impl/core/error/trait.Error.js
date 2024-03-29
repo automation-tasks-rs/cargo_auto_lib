fa262e30a5d8807250594fd65fcff070d26c712f
bestia.dev <info@bestia.dev>
2024-03-29 18:08:12 +0000

    update docs

--- docs/trait.impl/core/error/trait.Error.js
+++ docs/trait.impl/core/error/trait.Error.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"cargo_auto_lib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.76.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"cargo_auto_lib/error_mod/enum.LibError.html\" title=\"enum cargo_auto_lib::error_mod::LibError\">LibError</a>"]]
+"cargo_auto_lib":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.77.0/core/error/trait.Error.html\" title=\"trait core::error::Error\">Error</a> for <a class=\"enum\" href=\"cargo_auto_lib/error_mod/enum.LibError.html\" title=\"enum cargo_auto_lib::error_mod::LibError\">LibError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
