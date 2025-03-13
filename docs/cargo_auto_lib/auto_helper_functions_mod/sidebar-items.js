82644c95fd9a90b771a051c5ce96f1f6e3e3d10d
bestia.dev <info@bestia.dev>
2025-03-13 15:41:15 +0000

    update docs

--- docs/cargo_auto_lib/auto_helper_functions_mod/sidebar-items.js
+++ docs/cargo_auto_lib/auto_helper_functions_mod/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["completion_return_one_or_more_sub_commands","exit_if_not_run_in_rust_project_root_directory","home_dir"]};
+window.SIDEBAR_ITEMS = {"fn":["completion_return_one_or_more_sub_commands","exit_if_not_run_in_rust_project_root_directory","home_dir","tilde_expand_to_home_dir_utf8"]};
